30fa2559e4982c8d53aa37bdd9e6b2a542579f0a
Adam Bahrawy <adambahrawy750@gmail>
2025-03-01 19:56:05 -0800

    Initial Commit

---
+++ SimpleBankingSystem/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SimpleBankingSystem
+//
+//  Created by Ocelot B. on 3/1/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
